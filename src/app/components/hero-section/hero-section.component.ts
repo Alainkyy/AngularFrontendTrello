01e755bbb137a6babf6ab49c6f11d5a6e26c0b3f
Alain Yechouti <Alain.yechouti@gmail.com>
2023-09-01 19:39:47 +0200

    Avancement

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -7,17 +7,18 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 })
 export class HeroSectionComponent implements OnInit {
   currentHero: string = 'hero-green'; // Bouton vert actif par défaut
+  heroText: string = ''; // Texte dynamique du héros
+
   constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
     // Définir l'image comme image initiale
-    this.changeHero('hero-green');
+    this.changeHero('hero-green', 'var(--color-green)'); // Vous pouvez spécifier la couleur ici
   }
 
-
-  changeHero(imageId: string) {
+  changeHero(imageId: string, color: string) {
     this.currentHero = imageId;
-    // Masquer toutes les images du hero
+    this.updateCircleColor(color);
     this.hideAllHeroes();
 
     // Afficher l'image correspondant au bouton cliqué
@@ -25,6 +26,16 @@ export class HeroSectionComponent implements OnInit {
     if (element) {
       this.renderer.setStyle(element, 'display', 'block');
     }
+
+    // Mettre à jour le texte en fonction du bouton cliqué
+    this.updateHeroText(imageId);
+  }
+
+  private updateCircleColor(color: string) {
+    const circles = document.querySelectorAll('.circle');
+    circles.forEach(circle => {
+      this.renderer.setStyle(circle, 'background-color', color);
+    });
   }
 
   private hideAllHeroes() {
@@ -35,5 +46,26 @@ export class HeroSectionComponent implements OnInit {
         this.renderer.setStyle(element, 'display', 'none');
       }
     });
+  }
+
+  private updateHeroText(imageId: string) {
+    // Mettre à jour le texte en fonction de l'image cliquée
+    switch (imageId) {
+      case 'hero-orange':
+        this.heroText = 'Votre parcours village de l’emploi est financé\n par nos partenaires,qui sont aussi vos futurs\n employeurs.';
+        break;
+      case 'hero-blue':
+        this.heroText = 'Un emploi garanti chez les plus grands comptes, avec\n à la clé un gain d’expérience et une rémunération\n considérable.';
+        break;
+      case 'hero-pink':
+        this.heroText = 'Dès que vous prenez la décision de vous engager\n avec nous, vous êtes automatiquement pris en\n charge par le staff village de l’emploi pour\n franchir dûment toutes les étapes de votre\n parcours.';
+        break;
+      case 'hero-green':
+        this.heroText = 'Des Formateurs experts sont à votre service pour\n une transmission descompétences et techniques\n nécessaires pour une accélération de votre cursus\n professionnel.';
+        break;
+      default:
+        this.heroText = '';
+        break;
+    }
   }
 }
