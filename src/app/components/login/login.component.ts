47324df12695dfa5e3efe58fd3606a18ed36218b
Alain Yechouti <Alain.yechouti@gmail.com>
2023-09-05 17:50:07 +0200

    Bug login ==

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Consultant } from '../../models/Consultant';
 import { LoginService } from 'src/app/services/login.service';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -11,34 +10,37 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  consultants: any[] = [];
+  consultants: Consultant[] = [];
+  username: string = ''; 
+  password: string = ''; 
+  loginError: boolean = false; 
 
   constructor(
     private loginService: LoginService,
     private route: ActivatedRoute,
     private router: Router,
-    //private sharedService: SharedService,
     private cd: ChangeDetectorRef
   ) {}
 
-ngOnInit(): void {
-  this.route.queryParamMap.subscribe(params => {
-      this.afficherTousLesConsultants();
-  })};
+  ngOnInit(): void {
+    this.loginService.getConsultant().subscribe((data: Consultant[]) => {
+      this.consultants = data;
+      console.log('Liste Consultants :', this.consultants);
+    });
+  }
 
-  afficherTousLesConsultants() {
-    this.loginService.getConsultant().subscribe((result: any[]) => {
-      this.consultants = result.map(ConsultantData => new Consultant(
-        ConsultantData.idConsultant,
-        ConsultantData.nomConsultant,
-        ConsultantData.codeConsultant,
-        ConsultantData.statutConsultant,
-        ConsultantData.mDPConsultant,
-        ConsultantData.idAxe,
-        ConsultantData.idSpecialite,
-        ConsultantData.score
-      ));
-      console.log('Liste des consultants:', this.consultants);
-  });
-}
+  onLoginSubmit(): void {
+    const userExists = this.consultants.some(
+      (consultant) =>
+        consultant.codeConsultant === this.username && consultant.mDPConsultant === this.password
+    );
+  
+    if (userExists) {
+      this.router.navigate(['/home']);
+      console.log("Success");
+    } else {
+      this.loginError = true;
+      console.log("Fail");
+    }
+  }
 }
